af2364d5b22f8356d614d4d714a608d6dec5cc8d
Luiz Gustavo <luizg0509@gmail.com>
2024-10-18 17:35:56 -0300

    REPL enhanced

--- inter/inter.go
+++ inter/inter.go
@@ -7,17 +7,34 @@ import (
 	"strings"
 )
 
+type Status uint8
+
+const (
+	EXIT Status = iota
+	SUCCESS
+	UNRECOGNIZED
+)
+
 // REPL(run in go routine in main)
 func ReplCreate() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		PrintPrompt()
 		input := ReadPrompt(reader)
-
-		if input == "exit" {
-			return
+		if input[0] == '.' {
+			// TODO: Fazer Outros MetaComandos
+			if MetaCommand(input) == EXIT {
+				return
+			} else {
+				fmt.Printf("Comando não reconhecido '%s'\n", input)
+				continue
+			}
+		}
+		Comando, status := CommandStatement(input)
+		if status == SUCCESS {
+			ExecuteStatement(&Comando)
 		} else {
-			fmt.Printf("Comando: %s\n", input)
+			fmt.Printf("Comando não reconhecido '%s'\n", input)
 		}
 	}
 }
@@ -31,4 +48,47 @@ func ReadPrompt(b *bufio.Reader) string {
 	// TODO: Talvez adicionar outros tratamentos no string aqui
 	i = strings.Trim(i, "\n")
 	return i
+}
+
+type StatementType uint8
+
+const (
+	INSERT StatementType = iota
+	SELECT
+	NONE
+)
+
+type Statement struct {
+	Type StatementType
+}
+
+func MetaCommand(s string) Status {
+	switch s {
+	case ".exit":
+		return EXIT
+	}
+	return UNRECOGNIZED
+}
+
+func StartWith(s, p string) bool {
+	return strings.HasPrefix(s, p)
+}
+
+// TODO: Melhorar essa comparação
+func CommandStatement(s string) (Statement, Status) {
+	if StartWith(s, "insert") {
+		return Statement{Type: INSERT}, SUCCESS
+	} else if StartWith(s, "select") {
+		return Statement{Type: SELECT}, SUCCESS
+	}
+	return Statement{Type: NONE}, UNRECOGNIZED
+}
+
+func ExecuteStatement(s *Statement) {
+	switch s.Type {
+	case INSERT:
+		fmt.Println("Inserindo")
+	case SELECT:
+		fmt.Println("Selecionando")
+	}
 }
